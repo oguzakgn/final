bb1e9806d86aace53f04086d4a605cbc72cdadd7
oguzakgn <142172423+oguzakgn@users.noreply.github.com>
2024-01-21 21:38:14 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+var x = 10;
+
+console.log(x);
+
+let y = 20;
+
+console.log(y);
+
+
+const z= 30;
+
+console.log(z);
+
+
+
+let fruits = ["Apple","Banana","Orange","Mango"];
+
+for(let i = 0; i<fruits.length;i++) {
+    console.log(fruits[i]);
+}
+
+
+
+let person = {
+firstName : "Oguz",
+lastName :"Akgun",
+age: 21,
+hobbies: ["Chess","Reading"],
+address :{
+street:"Kırgız",
+city:"Istanbul",
+country:"Turkıye"
+}
+};
+
+console.log(person.firstName);
+console.log(person.age);
+console.log(person.hobbies);
+console.log(person.address);
+
+
+let hour = 18;
+
+if(hour>12) {
+    console.log("Good Morning");
+
+    
+}else{ console.log("Good Afternoon")}
+
+
+
+let age = 25;
+
+if(age>=18){
+if(age<21){
+
+    console.log("deneme 1");
+
+}
+
+else{
+    console.log("deneme 2");
+
+}
+
+}else{
+    console.log("deneme 3");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
